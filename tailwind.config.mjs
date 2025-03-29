e1f8171d17accfc214e00d5263f67b2cd8e2a186
Emily Olszewski <92195356+emilyoski@users.noreply.github.com>
2025-03-28 18:50:58 -0700

    fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,39 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+import defaultTheme from 'tailwindcss/defaultTheme';
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+import daisyuiThemes from 'daisyui/src/theming/themes';
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite/**/*.js'],
-	daisyui: {
-		themes: [
-			{
-				lofi: {
-					...require("daisyui/src/theming/themes")["lofi"],
-					"--rounded-box": "1rem",
-					"--rounded-btn": "0.5rem",
-					"--rounded-badge": "1.9rem"
-				},
-				black: {
-					...require("daisyui/src/theming/themes")["black"],
-					"--rounded-box": "1rem",
-					"--rounded-btn": "0.5rem",
-					"--rounded-badge": "1.9rem",
-					"secondary": "#e5e5e5"
-				}
-			}
-		],
-	},
-	theme: {
-		extend: {
-			fontFamily: {
-				'sans': ["DM Sans", "Inter", ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [require("@tailwindcss/typography"), require("daisyui")],
-}
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js',
+  ],
+  daisyui: {
+    themes: [
+      {
+        lofi: {
+          ...daisyuiThemes['lofi'],
+          '--rounded-box': '1rem',
+          '--rounded-btn': '0.5rem',
+          '--rounded-badge': '1.9rem',
+        },
+        black: {
+          ...daisyuiThemes['black'],
+          '--rounded-box': '1rem',
+          '--rounded-btn': '0.5rem',
+          '--rounded-badge': '1.9rem',
+          secondary: '#e5e5e5',
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [typography, daisyui],
+};
