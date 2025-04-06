c574056c984ed7e24a69e2cb80dee25df8fe0145
Emily Olszewski <92195356+emilyoski@users.noreply.github.com>
2025-04-06 12:07:45 -0700

    don't direct to dev url yet

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.emilyolszewski.dev',
   integrations: [tailwind(), icon(), compress()],
   output: 'server',
   adapter: vercel(),
