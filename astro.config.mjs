b5488384101f59a8b9799f5e9e27a8c0e7f2a15b
Emily Olszewski <92195356+emilyoski@users.noreply.github.com>
2025-04-06 12:04:49 -0700

    add deployment things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
-import icon from "astro-icon";
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.emilyolszewski.dev',
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel(),
 });
